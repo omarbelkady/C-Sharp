fd0fa42261735e986ace82653843d2d9698bc643
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-07 20:29:23 +0100

    Create Adding_nd_Casting.cs

---
+++ Casting/Adding_nd_Casting.cs
@@ -0,0 +1,19 @@
+using System;
+namespace Ramo
+{
+  	class Program
+	{
+        static void Main(string[] args)//
+        {
+	
+    		Console.Write("Enter a number: ");
+    		int num1=Convert.ToInt32(Console.ReadLine());
+    		Console.Write("Enter a 2nd number: ");
+    		int num2=Convert.ToInt32(Console.ReadLine());
+    		Console.Write(num1+num2);
+    		
+    		Console.ReadLine();
+        }
+
+    }
+}	
