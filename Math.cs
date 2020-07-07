0744eccb680bc10050c2aeaaec0e34577d39be71
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-07 19:24:11 +0100

    Create Math.cs

---
+++ Math.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Ramo
+{
+    class Program
+	{
+        static void Main(string[] args)//Method or function
+        {
+        			Console.WriteLine(Math.Abs(-40));
+        			Console.WriteLine(Math.Pow(3,2));
+        			Console.ReadLine();
+        }
+   
+    }
+}
