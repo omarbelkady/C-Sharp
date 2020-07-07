93d33a24f60ef2ed58c3621c4eaa7c40eb3a33e4
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-07 19:10:12 +0100

    Create WorkingWithStrings.cs

---
+++ Strings/WorkingWithStrings.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+class Program
+{
+    public static void Main(string[] args)//Method or function
+    {
+    	string phrase= "Omar Belkady";
+    	Console.WriteLine(phrase);	
+    	
+    	Console.WriteLine(phrase.Contains("Belkady"));
+        
+        Console.ReadLine();
+        
+
+    }
+
+}
