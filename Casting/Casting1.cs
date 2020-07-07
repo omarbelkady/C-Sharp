75b683f661a5a63862db64d61dbdb392f94580ae
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-07 20:23:01 +0100

    Create Casting1.cs

---
+++ Casting/Casting1.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Ramo
+{
+  	class Casting1
+	{
+        static void Main(string[] args)//
+        {
+    		int num=Convert.ToInt32("45");
+    		Console.Write(num+6);
+    		Console.ReadLine();
+        }
+
+    }
+}	
