029dc76c9efc6b653f621e6ac09198c00a78f68b
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-07 20:19:50 +0100

    Create Userinput.cs

---
+++ Userinput.cs
@@ -0,0 +1,17 @@
+using System;
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)//Method or function
+        {  
+			Console.Write("Enter your name: ");
+			string name= Console.ReadLine();
+			Console.Write("Enter your age: ");
+			int age= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello " + name +" you are "+ age+" years old");
+            Console.ReadLine();
+        }
+       
+    }
+}
