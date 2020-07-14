a89ba6c358377fa9c5fadef138e86d0be27b8cb3
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-14 01:19:01 +0100

    Create Individual.cs

---
+++ Conditionals/Individual.cs
@@ -0,0 +1,38 @@
+using System;
+namespace Classes{
+    public class Individual
+    {
+        //We can put properties, fields, methods, etc. here
+        //I will create a property of type boolean isAnnoying
+        //Properties store things and are used to access data from the outside
+        //A Field is used to store data in the inside
+        //Field naming is always camel case
+        //MEthod naming is pascal case
+        
+        public string FName{get; set;}//property A
+        public string LName{get; set;}//property B
+        
+        public void isAnnoying(bool val)
+        {
+            if(val){
+                Console.WriteLine(FName+" "+LName+" is Annoying");
+            }
+        }
+        
+        
+        //property
+        public string retFullNam{
+            get{
+                return FName+" "+LName;
+            }
+        }
+        
+        //This is a method it does sth
+        public string myFullN(){
+            return FName+" "+LName;
+        }
+        
+        
+    }
+    
+}
