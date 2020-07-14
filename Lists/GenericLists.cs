e71eb59b1a37ae1a58bf925f12f7a1c6a32c321a
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-14 21:53:31 +0100

    Create GenericLists.cs

---
+++ Lists/GenericLists.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+namespace myAwesomeProg{
+    class GenericLists {
+        static void Main() {
+            
+            //<============How To Create A List===========>
+            /*How to initialize a list with values*/
+            List<int> bestYears= new List<int>(){2004,2005,2006,2007};
+            //new List Keyword is mandatory for initialization of a list
+            
+            
+        }
+        
+        
+        static void Wr_L(object o)
+        {
+            Console.WriteLine(o);
+        }
+        
+        static void Wr_i(object m)
+        {
+            Console.Write(m);
+        }
+        
+        
+        
+    }
+}
