5abdd3232bef5d88df8f05ce98fc521bb0785719
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-14 01:52:05 +0100

    Create main.cs

---
+++ Conditionals/PartII/main.cs
@@ -0,0 +1,36 @@
+using System;
+namespace Classes{
+    class main
+    {
+        static void Main()
+        {
+            //Individual eliya;
+            //The above line of code will generate a null reference exception
+            //because the constructor was not called
+            Individual martin= new Individual();
+            
+            martin.FName="Martin";
+            martin.LName="Smith";
+            
+            
+            Console.Write("Make An Educated Guess of the full Name of the Individual: ");
+            string FullNamGuess= Console.ReadLine();
+            
+            Console.Write("Make A 2nd Educated Guess: ");
+            string FullNamGuess2= Console.ReadLine();
+            
+            if(martin.retFullNam==FullNamGuess)
+            {
+                Console.WriteLine("Spot On!");
+            }
+            else if(martin.retFullNam==FullNamGuess2)
+            {
+                Console.WriteLine("2nd Try but hey at least you got it");
+            }
+            
+            else{
+                Console.WriteLine("Just quit! You do not have the sufficient level!");
+            }
+        }
+    }
+}
