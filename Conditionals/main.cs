05b8668848267ade782a94a0fdfe6640eb644f34
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-14 01:17:51 +0100

    Create main.cs

---
+++ Conditionals/main.cs
@@ -0,0 +1,23 @@
+using System;
+namespace Classes{
+    class main
+    {
+        static void Main()
+        {
+            //Individual eliya;
+            //The above line of code will generate a null reference exception
+            //because the constructor was not called
+            Individual martin= new Individual();
+            
+            martin.FName="Martin";
+            martin.LName="Smith";
+            martin.isAnnoying(true);
+            
+            if(martin.retFullNam=="Martin Smith")
+            //In java this would be if(martin.retFullNam.equals("Martin Smith"))
+            {
+                Console.WriteLine("Correct Person yo!");
+            }
+        }
+    }
+}
