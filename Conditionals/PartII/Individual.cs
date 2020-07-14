c9acf2d10b930822f102232014bbf4064ab69973
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-14 01:52:46 +0100

    Create Individual.cs

---
+++ Conditionals/PartII/Individual.cs
@@ -0,0 +1,21 @@
+using System;
+namespace Classes{
+    public class Individual
+    {
+        
+        public string FName{get; set;}//property A
+        public string LName{get; set;}//property B
+        
+        //property
+        public string retFullNam{
+            get{
+                return FName+" "+LName;
+            }
+        }
+        
+        //This is a method it does sth
+        public string myFullN(){
+            return FName+" "+LName;
+        }
+    }
+}
