3ea6495b2ac61bfd531693f9a03de8c31fc575bf
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-15 15:41:25 +0100

    Create ForEachII.cs

---
+++ Loops/ForEachII.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyAwesomeProg{
+    class ForEachII
+    {
+        static void Main() 
+        {
+        
+            List<int> bestPlayersNums=new List<int>(){
+                //"Messi","Pele","Maradona","Ronadinho","Ronaldo","R.Carlos","Figo","Modric", "Hazard","Suarez"
+                10,10,10,10,9,3,7,10,7,9
+            };
+            foreach(int num in bestPlayersNums)
+            {
+                Wr_i(num+" ");
+            }
+        
+        }
+        static void Wr_L(object o)
+        {
+            Console.WriteLine(o);
+        }
+        
+        static void Wr_i(object m)
+        {
+            Console.Write(m);
+        }
+    }
+}
