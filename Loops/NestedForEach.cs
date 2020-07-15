ffe653af2db12dbcfca5b5a4dd1f1c9997d019e4
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-15 16:24:15 +0100

    Create NestedForEach.cs

---
+++ Loops/NestedForEach.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace NestedForEach{
+    class List
+    {
+        static void Main() 
+        {
+        
+            List<List<int>> PlayerNumbers=new List<List<int>>()
+            {
+                //"Messi","Pele","Maradona","Ronadinho","Ronaldo","R.Carlos","Figo","Modric", "Hazard","Suarez"
+                new List<int>{10,10,10}, //newList instead of new List will generate an unexpected Symbol error
+                new List<int>{10,9,3},
+                new List<int>{7,10,7,9}
+            };
+            foreach(List<int> nums in PlayerNumbers.ToArray())
+            {
+                foreach(int num in nums)
+                {    
+                    Wr_i(num+"\t");
+                }
+                Wr_L(" ");
+            }
+        
+        }
+        static void Wr_L(object o)
+        {
+            Console.WriteLine(o);
+        }
+        
+        static void Wr_i(object m)
+        {
+            Console.Write(m);
+        }
+    }
+}
