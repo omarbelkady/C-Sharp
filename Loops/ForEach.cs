3e67e5e2a13aa410ae7460613d1ea3420b9cf04a
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-15 15:27:01 +0100

    Create ForEach.cs

---
+++ Loops/ForEach.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ForEach{
+    class List
+    {
+        static void Main() 
+        {
+        
+            List<string> bestPlayers=new List<string>(){
+                "Messi","Pele","Maradona","Ronadinho","Ronaldo","R.Carlos","Figo","Modric", "Hazard","Suarez"
+            };
+            foreach(string player in bestPlayers)
+            {
+                Wr_L(player);
+            }
+        
+        }
+        static void Wr_L(object o)
+        {
+            Console.WriteLine(o);
+        }
+        
+        static void Wr_i(object m)
+        {
+            Console.Write(m);
+        }
+    }
+}
