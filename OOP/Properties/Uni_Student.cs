a0826774c73020019da8689efac56b080e66472c
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-16 14:09:54 +0100

    Create Uni_Student.cs

---
+++ OOP/Properties/Uni_Student.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Properties
+{
+    public class Uni_Student
+    {
+        //I create a property
+        //public object MyProp{get;set;}
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+        public int Identification_Number {get;set;}
+        public string Major {get;set;}
+        public int Year {get;set;}
+    }
+}
