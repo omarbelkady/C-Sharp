4515b044a07f114b14affbcda7cb6147efc2c026
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-16 21:09:56 +0100

    Create Uni_Student.cs

---
+++ OOP/Cust_GET_SET/Uni_Student.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace CustGetAndSet
+{
+    public class Uni_Student
+    {
+        static void Wr_L(object o)=>Console.WriteLine(o);//Includes a new line char
+        
+        static void Wr_i(object m)=>Console.Write(m);//Includes a new line char
+        
+        public string FName{get;set;}
+        public string LName {get;set;}
+        
+        public string TheFullName{
+            get{
+                return FName+" "+LName;
+            }
+        }
+        
+        public string Major {get;set;}
+        public int Year {get;set;}
+        
+        
+        public string outingToCons(int num)
+        {
+            string msg="";
+            for(int j=0;j<num;j++)
+            {
+                msg += FName+ " " +LName + " "+ Year+"\n";
+            }
+            return msg;
+        }
+        
+        
+        
+    }
+}
