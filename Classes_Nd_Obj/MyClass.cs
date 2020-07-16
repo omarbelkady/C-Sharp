320ddced6aaae4760855d2cd1deea566d53cf793
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-16 11:16:14 +0100

    Create MyClass.cs

---
+++ Classes_Nd_Obj/MyClass.cs
@@ -0,0 +1,11 @@
+using System;
+namespace myAwesomeProg{
+    public class MyClass
+    {
+        public MyClass() //The constructor
+        {
+            Console.WriteLine("Created");
+        }
+    }
+}
+//Unexpected symbol `end-of-file` means you are missing a curly brace
