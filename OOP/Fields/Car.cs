36b9468490f6f6e4b8ed916fa4c4e99016c0d238
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-16 11:46:21 +0100

    Create Car.cs

---
+++ OOP/Fields/Car.cs
@@ -0,0 +1,12 @@
+using System;
+namespace FieldMaking{
+    public class Car
+    {
+        public string Manufacturer;//Field A
+        public string Model;//Field B
+        public int Year;//Field C
+        
+        //when using a property make sure to use private instead of public
+        //Use properties over fields
+    }
+}
