53659489191c3af28b80d0eff75805d270bb4b6a
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-16 15:40:29 +0100

    Create Uni_Student.cs

---
+++ Classes_Meths_Props/Void_Method/Uni_Student.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Meths
+{
+    public class Uni_Student
+    {
+        static void Wr_L(object o)=>Console.WriteLine(o);//Includes a new line char
+        
+        static void Wr_i(object m)=>Console.Write(m);//Includes a new line char
+        
+        //I create a property
+        //public object MyProp{get;set;}
+        public string FirstName {get;set;}
+        public string LastName {get;set;}
+        public int Identification_Number {get;set;}
+        public string Major {get;set;}
+        public int Year {get;set;}
+        
+        public void PrintingSth()//Method Naming is Pascal Case first Letter of Every Word is Capital
+        {
+             Wr_L(FirstName+" "+LastName+" has an id of: "+Identification_Number+" and majors in: "+Major+" and is in his "+Year+"(th) year of University");
+        }
+    }
+}
