dc5b77d2af85e6323281bc865ad68bad2ea20200
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-16 15:39:51 +0100

    Create main.cs

---
+++ Classes_Meths_Props/Void_Method/main.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Meths
+{
+    class MyProg
+    {
+        static void Wr_L(object o)=>Console.WriteLine(o);//Includes a new line char
+        
+        static void Wr_i(object m)=>Console.Write(m);//Includes a new line char
+        
+        static void Main(){
+            MyProg firstProg = new MyProg();//Calling the constructor
+            firstProg.printing();
+        }
+        
+        
+        
+        public void printing()
+        {
+            Uni_Student Alan= new Uni_Student();
+            Alan.FirstName = "Alan";
+            Alan.LastName= "Ngo";
+            Alan.Identification_Number= 634256837;
+            Alan.Major= "CS with a C-Language Concentration!";
+            Alan.Year=4;
+            Alan.PrintingSth();
+        }
+    }
+}
