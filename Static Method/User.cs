80b884b80c2ac423feee467495fbb358a340f421
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-17 10:28:26 +0100

    Create User.cs

---
+++ Static Method/User.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace Static_Methods
+{
+    public class User
+    {
+        static void Wr_L(object o)=>Console.WriteLine(o);//Includes a new line char
+        
+        static void Wr_i(object m)=>Console.Write(m);//Includes a new line char
+        
+        public string TheFullName{
+            get{
+                return FName+" "+LName;
+            }
+        }
+        
+        public string FName{get;set;}
+        public string LName {get;set;}
+        
+        //Creating the static Method
+        public static void PrintMe(User user)
+        {
+            Wr_L("Static Method");
+            user.outingToCons(2);
+        }
+         
+        public string outingToCons(int num)
+        {
+            string msg="";
+            for(int j=0;j<num;j++)
+            {
+                msg += FName+ " " +LName+"\n";
+            }
+            return msg;
+        }
+    }
+}
