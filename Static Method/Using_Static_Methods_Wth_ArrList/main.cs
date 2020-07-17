4b7813d745d99f7c9ad8d538221782ee9a8d6e9b
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-17 11:14:16 +0100

    Create main.cs

---
+++ Static Method/Using_Static_Methods_Wth_ArrList/main.cs
@@ -0,0 +1,42 @@
+//When Something is Static it is attached to the Class rather than
+//an instance of a class/Object. An instance method is attached to the instances of the class/objects of the class. 
+//We define a class and then we create instances of the class
+//which are known as objects. When you define a method in the class. Everytime you
+//instantiate the class into an object the method is made available
+using System;
+using System.Collections.Generic;
+
+
+namespace Static_Methods
+{
+    class MyProg
+    {
+        static void Wr_L(object o)=>Console.WriteLine(o);//Includes a new line char
+        
+        static void Wr_i(object m)=>Console.Write(m);//Includes a new line char
+        
+        static void Main(){
+            MyProg firstProg = new MyProg();//Creating an object
+            firstProg.printing();
+        }
+        
+        public void printing()
+        {
+            User max= new User();//Instance identifier
+            max.FName= "Max";//Identifier.methodName
+            max.LName= "Versatile";
+            
+            User richard= new User();//Instance identifier
+            richard.FName= "Richard";//Identifier.methodName
+            richard.LName= "Jones";
+            
+            
+            List<User> theUsers= new List<User>();
+            theUsers.Add(max);
+            theUsers.Add(richard);
+            User.PrintThemAndMe(theUsers);
+        }
+        //Anytime we pass in objects they are modifiable
+        //We can change objects but we cannot assign new objects
+    }
+}
