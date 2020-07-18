bd567e1f44b3a281788b900ac0100f9452d28535
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-18 15:35:08 +0100

    Create Professor.cs

---
+++ Polymorphism/Professor.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Polymorphism
+{
+    public class Professor : User{
+        
+        static void Wr_L(object o)=>Console.WriteLine(o);//Includes a new line char
+        
+        static void Wr_i(object m)=>Console.Write(m);//Includes a new line char
+        
+        //Not putting the parenthesis for the constructor will result in the error:
+        //Unexpected symbol `{' in class, struct, or interface member declaration
+        public Professor(){
+            
+        }
+        
+        public override void overrideThisPlease()
+        {
+            Wr_L("Howdy Howdy y'all my I just overrode the User meth and by I, I am a Prof and my name is "+TheFullName);
+        }
+    }
+}
