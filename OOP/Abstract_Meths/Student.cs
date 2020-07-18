f3a4fdb9ef12793c685c8db8a2fed468b391de42
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-18 14:14:42 +0100

    Create Student.cs

---
+++ OOP/Abstract_Meths/Student.cs
@@ -0,0 +1,19 @@
+using System;
+namespace Abstract_Methods
+{
+    //If you want to prevent people from creating Users directly
+    //By forcing them to make a Student, Teacher, employee. You make the class abstract
+    //If it is done then you will get an error: Cannot create an instance of the abstract class
+    public class Student : User //Student Inherits From User
+    {
+        static void Wr_L(object o)=>Console.WriteLine(o);//Includes a new line char
+        
+        static void Wr_i(object m)=>Console.Write(m);//Includes a new line char 
+        
+        //Inherit and Override
+        public override void overrideThisPlease()
+        {
+            Wr_L("Howdy Howdy y'all my I just overrode the User meth and by I, I am: "+TheFullName.ToUpper());
+        }
+    }
+}
