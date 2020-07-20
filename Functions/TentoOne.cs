ada360d0830f6fc9fefcbe622c7287071f01a955
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-20 22:12:51 +0100

    Create TentoOne.cs

---
+++ Functions/TentoOne.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TentoOne{
+    class TentoOne{
+        public static void RunMyProg()
+        {
+            for(int num=10;num>1;num--)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        
+        
+        static void Main()
+        {
+            RunMyProg();
+        }
+    }
