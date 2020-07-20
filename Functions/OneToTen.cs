ba2f35a2585c03cb06f54e02da2b79dd66648798
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-20 22:07:48 +0100

    Create OneToTen.cs

---
+++ Functions/OneToTen.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace oneToTen{
+    class oneToTen{
+        public static void RunMyProg()
+        {
+            for(int num=1;num<=10;num++)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        
+        static void Main()
+        {
+            RunMyProg();
+        }
+    }
+}
