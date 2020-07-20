7a73ce6449adbc95ee640185c5e05d1b629cd8f4
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-20 23:16:24 +0100

    Create Date_Display.cs

---
+++ Functions/Date_Display.cs
@@ -0,0 +1,21 @@
+//This program converts a 24-hour clock to 12-hour clock
+using System;
+using System.Globalization;
+
+namespace Date_Display{
+    public class Date_Display{
+        
+        public static void RunMyProg()
+        {
+            var date1 = new DateTime(2020, 07, 20, 23, 14, 40);
+            Console.WriteLine(date1.ToString());
+            
+        }
+        
+        public static void Main(string [] args)
+        {
+            RunMyProg();
+        }
+            
+    }
+}
